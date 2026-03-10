77507e43ea581693a090fa12367f9469bd35506f
Christian <ChristianHasan0@outlook.com>
2026-03-10 16:45:47 +0100

    Add files via upload

---
+++ GatewayClientErrors.py
@@ -0,0 +1,14 @@
+class InvalidTokenError(Exception):
+    pass
+
+class UnhandledClosedConnectionCodeError(Exception):
+    pass
+
+class InvalidBotIntents(Exception):
+    pass
+
+class UnassingedCallbackFunctionError(Exception):
+    pass
+
+class ServerDownError(Exception):
+    pass
