d0e388ca301c4bc35395acb27039530c545761ba
Christian <ChristianHasan0@outlook.com>
2026-03-13 10:28:39 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from main import Setup
-from betterbot.gateway_client.event_related.events import Events
-from betterbot.rest_client.rest_handlers.interaction import InteractionTypes
-from betterbot.rest_client.error import Types
