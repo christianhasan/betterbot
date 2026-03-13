f816f7378a2fc9e60e438f15ad76937a3396c40b
Christian <ChristianHasan0@outlook.com>
2026-03-13 10:21:34 +0100

    Delete gateway_client directory

--- gateway_client/connection/session.py
+++
@@ -1,7 +0,0 @@
-class Session:
-    def __init__(self):
-        self.sequence = None
-        self.session_id = None
-        self.token = None
-        self.intents = None
-        self.heartbeat_interval = None
