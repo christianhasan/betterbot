0627fafa34b1c724f3915632aa7ea43373f452b5
Christian <ChristianHasan0@outlook.com>
2026-03-13 10:29:01 +0100

    Add files via upload

---
+++ betterbot/__init__.py
@@ -0,0 +1,4 @@
+from main import Setup
+from betterbot.gateway_client.event_related.events import Events
+from betterbot.rest_client.rest_handlers.interaction import InteractionTypes
+from betterbot.rest_client.error import Types
